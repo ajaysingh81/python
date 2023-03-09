b5884856e6ab28117187e9498a267f482099d337
ajaysingh81 <singhajay22036@gmail.com>
2023-03-09 21:03:11 +0530

    some changes in this file

--- python_basic/upeer.py
+++ python_basic/upeer.py
@@ -5,9 +5,8 @@
 # print(skill)
 
 # # this is not working
-
-# var = {1,True}
-# print(var)
+var = {1,True}
+print(var)
 
 
 # class Hello:
@@ -19,7 +18,7 @@
 # num = "skil Sequel"*3*3
 # print(num)
 
-# print(10+2-13*3/2%2)
+print(10+5-13*3/2%2)
 
 
 
@@ -28,5 +27,39 @@
 #     print(j,end=" ")
 
 
+Hello1 = ['XX', 'YY']
+for i in Hello1:
+    i.lower()
+print(Hello1)
+
+str1 = "Skill"
+str2 = ":"
+str3 = "Sequel"
+str1[-1:]
 
 
+Skill = ['ab', 'pure']
+
+for i in Skill:
+    i.upper()
+
+print(Skill)
+
+Num = 1
+while True:
+    if Num % 14 == 0:
+        break
+    print(Num)
+    Num+=1
+
+var = 1
+while True:
+    if var % 5 == 0:
+        break
+    print(var,end = ' ')
+    var +=1
+
+var1 = 4
+var2 = 4
+var3 = "8"
+print(var1+var2+var3)
